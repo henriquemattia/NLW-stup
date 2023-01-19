fcaad5eb50abe84de19f123b004291709854158c
henriquemattia <mattia.henrique@gmail.com>
2023-01-19 19:39:35 -0300

    Estilizando Front-end Web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,18 @@
 import './styles/global.css'
+import { Plus } from 'phosphor-react'
+import { Header } from './components/Header'
+// import { Habit } from "./components/Habit"
 
-import { Habit } from "./components/Habit"
 
-function App() {
+export function App() {
   return (
-    <div className='habit'>
-     <Habit completed={3}/>
-     <Habit completed={10}/>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex felx-col gap-16'>
+
+        <Header />
+        
+
+      </div>
     </div>
   )
 }
-
-export default App
