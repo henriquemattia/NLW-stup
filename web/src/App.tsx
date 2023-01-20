d8f057260026de4f0b2388d8ecffd4ac51d18d53
henriquemattia <mattia.henrique@gmail.com>
2023-01-19 22:20:19 -0300

    Iniciando a criação de quadrados da tabela d dias

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
 import './styles/global.css'
 import { Plus } from 'phosphor-react'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 // import { Habit } from "./components/Habit"
 
 
 export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex felx-col gap-16'>
-
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header />
-        
-
+        <SummaryTable />
       </div>
     </div>
   )
