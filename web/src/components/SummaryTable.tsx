b8ef9a6755b907be80726fd7a1c6201fe17c71ed
henriquemattia <mattia.henrique@gmail.com>
2023-01-20 15:40:52 -0300

    Configurando Tailwindcss React-native

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBegnning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -9,6 +10,11 @@ const weekDays = [
     'S',
     'S'
 ]
+
+const summaryDates = generateDatesFromYearBegnning()
+
+const minimumSummaryDatesSize = 18 * 7
+const amountOfdaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
     return(
@@ -27,7 +33,18 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-              <HabitDay />
+                {summaryDates.map(date => {
+                    return <HabitDay key={date.toString()}/>
+                })}
+
+                {amountOfdaysToFill > 0 && Array.from({ length: amountOfdaysToFill}).map((_, i)=>{
+                    return (
+                        <div
+                            key={i}
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                         />
+                    )
+                })}
             </div>
         </div>
     )
