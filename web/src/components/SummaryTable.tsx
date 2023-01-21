d0e98300cb489c9948bd30f7d12cc010950e9214
henriquemattia <mattia.henrique@gmail.com>
2023-01-21 18:36:27 -0300

    Quadradinhos com cores variadas de progresso

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -17,33 +17,39 @@ const minimumSummaryDatesSize = 18 * 7
 const amountOfdaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
-    return(
+    return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
                 {weekDays.map((weekday, i) => {
                     return (
-                        <div key={`${weekday}-${i}`} 
-                        className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+                        <div key={`${weekday}-${i}`}
+                            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
                         >
                             {weekday}
                         </div>
                     )
                 })}
-                
+
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
-                    return <HabitDay key={date.toString()}/>
+                    return (
+                        <HabitDay
+                            key={date.toString()}   
+                            amount={5}
+                            completed={Math.round(Math.random() * 5)}
+                        />
+                    )
                 })}
-                
-                        {/* DIAS NÃO CLICAVEIS  */}
-                {amountOfdaysToFill > 0 && Array.from({ length: amountOfdaysToFill}).map((_, i)=>{
+
+                {/* DIAS NÃO CLICAVEIS  */}
+                {amountOfdaysToFill > 0 && Array.from({ length: amountOfdaysToFill }).map((_, i) => {
                     return (
                         <div
                             key={i}
                             className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-                         />
+                        />
                     )
                 })}
             </div>
